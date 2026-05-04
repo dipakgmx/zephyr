a5aee537f6f49cfd6bd67980991eb1d08ea575f0
Dipak Shetty <shetty.dipak@gmx.com>
2026-05-04 23:10:55 +0200

    samples: bluetooth: acs
    fixing+optimizing
    
    Signed-off-by: Dipak Shetty <shetty.dipak@gmx.com>

--- subsys/bluetooth/services/acs/acs_protected_resource_router.c
+++ subsys/bluetooth/services/acs/acs_protected_resource_router.c
@@ -61,58 +61,54 @@ static bool router_handle_in_cps(uint16_t map_id, uint16_t resource_handle)
 	struct bt_acs_restriction_map map;
 	struct router_cp_match_ctx ctx;
 
-	if (resource_handle == ACS_RMAP_FILTER_ALL) {
-		return true;
-	}
 	if (acs_rmap_lookup(map_id, &map) != 0) {
 		return false;
 	}
+
 	ctx.resource_handle = resource_handle;
 	ctx.found = false;
 	acs_rmap_foreach_cp(&map, router_cp_match_cb, &ctx);
 	return ctx.found;
 }
+
 #endif /* CONFIG_BT_ACS_FEAT_AUTHORIZATION */
 
 int acs_classify_frame(const struct acs_frame *frame, uint16_t map_id, struct acs_route *route)
 {
+	enum acs_route_kind kind;
 	if (!frame || !route) {
 		return -EINVAL;
 	}
 
 	if (frame->source_channel == ACS_SRC_CP) {
-		*route = (struct acs_route){
-			.kind = ACS_ROUTE_ACS_CP,
-			.resource_handle = 0,
-			.isc_id = 0,
-			.encrypted = false,
-		};
-		return 0;
-	}
-
+		kind = ACS_ROUTE_ACS_CP;
 #if defined(CONFIG_BT_ACS_FEAT_AUTHORIZATION)
-	if (router_handle_in_cps(map_id, frame->resource_handle)) {
-		*route = (struct acs_route){
-			.kind = ACS_ROUTE_PROTECTED_SERVICE_CP,
-			.resource_handle = frame->resource_handle,
-			.isc_id = frame->isc_id,
-			.encrypted = true,
-		};
-		return 0;
-	}
-
-	if (router_handle_in_chars(map_id, frame->resource_handle)) {
-		*route = (struct acs_route){
-			.kind = ACS_ROUTE_PROTECTED_CHAR,
-			.resource_handle = frame->resource_handle,
-			.isc_id = frame->isc_id,
-			.encrypted = true,
-		};
-		return 0;
-	}
+	} else if (router_handle_in_cps(map_id, frame->resource_handle)) {
+		kind = ACS_ROUTE_PROTECTED_SERVICE_CP;
+	} else if (router_handle_in_chars(map_id, frame->resource_handle)) {
+		kind = ACS_ROUTE_PROTECTED_CHAR;
 #else
-	ARG_UNUSED(map_id);
-#endif /* CONFIG_BT_ACS_FEAT_AUTHORIZATION */
+		ARG_UNUSED(map_id);
+#endif
+	} else {
+		LOG_WRN("Frame source channel %d is not CP or Data In", frame->source_channel);
+		return -ENOENT;
+	}
 
-	return -ENOENT;
+	switch (kind) {
+	case ACS_ROUTE_ACS_CP:
+		route->kind = kind;
+		break;
+	case ACS_ROUTE_PROTECTED_SERVICE_CP:
+	case ACS_ROUTE_PROTECTED_CHAR:
+		route->kind = kind;
+		route->resource_handle = frame->resource_handle;
+		route->isc_id = frame->isc_id;
+		route->encrypted = true;
+		break;
+	default:
+		__ASSERT_NO_MSG(false);
+		break;
+	}
+	return 0;
 }
